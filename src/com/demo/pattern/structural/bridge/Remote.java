66fd722b35f06609b5b620daff742b43879af1c8
ds-linh-nguyen <linh-nguyen@dimage.co.jp>
2022-04-20 15:42:47 +0700

    bridge - structural pattern

---
+++ src/com/demo/pattern/structural/bridge/Remote.java
@@ -0,0 +1,9 @@
+package com.demo.pattern.structural.bridge;
+
+public interface Remote {
+    void power();
+    void volumeDown();
+    void volumeUp();
+    void channelDown();
+    void channelUp();
+}
